d9f1dab5ccd4ed2985fd77601de0a9719e21d524
mrv0for0vandeta <saadjabli107@gmail.com>
2026-06-08 18:49:13 +0100

    Simplify Vercel configuration

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 // Vercel Serverless Function Entry Point
+require('dotenv').config();
+
+// Import the Express app
 const app = require('../backend/server');
 
-// Export for Vercel
-module.exports = app;
+// Export handler for Vercel serverless
+module.exports = async (req, res) => {
+    // Let Express handle the request
+    return app(req, res);
+};
